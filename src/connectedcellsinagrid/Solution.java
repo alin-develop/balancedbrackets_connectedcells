2d3181452a886e46650ddf3dcb802da64c5cff7e
alindev <linkalandarashvili@gmail.com>
2022-09-07 11:31:41 +0200

    Solution to HackerRank Problems: Balanced Brackets and Connected Cells in a Grid using Java 15.

---
+++ src/connectedcellsinagrid/Solution.java
@@ -0,0 +1,88 @@
+package connectedcellsinagrid;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'connectedCell' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY matrix as parameter.
+     */
+
+    public static int dfs(int i, int j, List<List<Integer>> matrix){
+
+        if (i<0 || j<0 || i>=matrix.size() || j>=matrix.size()) return 0;
+        if (matrix.get(i).get(j)==0) return 0;
+        int area = 1;
+        matrix.get(i).set(j, 0);
+        area += dfs(i+1, j, matrix);
+        area += dfs(i, j+1, matrix);
+        area += dfs(i+1, j+1, matrix);
+        area += dfs(i-1, j, matrix);
+        area += dfs(i, j-1, matrix);
+        area += dfs(i-1, j-1, matrix);
+        area += dfs(i+1, j-1, matrix);
+        area += dfs(i-1, j+1, matrix);
+        return area;
+    }
+
+    public static int connectedCell(List<List<Integer>> matrix) {
+        // Write your code here
+        int max = 0;
+        int result = -1;
+
+        for (int i = 0; i < matrix.size(); i++){
+            for (int j = 0; j < matrix.size(); j++ ){
+                if (matrix.get(i).get(j)==1) result = dfs(i, j, matrix);
+
+                if (result>max) max = result;
+            }
+        }
+        return max;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int m = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<List<Integer>> matrix = new ArrayList<>();
+
+        IntStream.range(0, n).forEach(i -> {
+            try {
+                matrix.add(
+                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt)
+                                .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int result = Result.connectedCell(matrix);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
